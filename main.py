2e4c8c51c27807171b5cfbde7501de9ce46f46fd
Іванченко Олександр <153652515+Olexander930@users.noreply.github.com>
2024-08-26 07:22:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+from mod_sum import sum_n
+n=int(input("Введіть значення n: "))
+print("Сума цифр числа: ", sum_n(n))
---
+++ mod_sum.py
@@ -0,0 +1,3 @@
+def sum_n (n):
+  n=abs(n)
+  return sum(int(digit) for digit in str(n))
